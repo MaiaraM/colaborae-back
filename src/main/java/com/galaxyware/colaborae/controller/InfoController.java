f6f2aa2efe8cabeeec5116e87174621ee7da9e96
Anselmo <ansjunior@msn.com>
2020-10-29 23:45:21 -0300

    criacao de repositories e controllers

--- src/main/java/com/galaxyware/colaborae/controller/InfoController.java
+++ src/main/java/com/galaxyware/colaborae/controller/InfoController.java
@@ -1,16 +1,12 @@
 package com.galaxyware.colaborae.controller;
 
-import com.galaxyware.colaborae.model.InfoAPIModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public ResponseEntity infos(){
         return ResponseEntity.status(HttpStatus.OK).body(new InfoAPIModel("Colaborae", "0.1"));
-    }
+    }*/
 }
