916c18eb0d973b5ef5500f5fef0f65a76c4b2278
Maiara Martins <maiaram@ciandt.com>
2020-11-26 00:06:40 -0300

    caminho estava errado

--- src/main/java/com/galaxyware/colaborae/config/WebSecurityConfig.java
+++ src/main/java/com/galaxyware/colaborae/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 //.antMatchers("/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
-                .antMatchers(HttpMethod.GET, "/users/**","/categories/**","}/services/**" ).permitAll()
+                .antMatchers(HttpMethod.GET, "/users/**","/categories/**","/services/**" ).permitAll()
 
                 .anyRequest().authenticated()
                 .and()
